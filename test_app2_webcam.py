757ed0f67e0a48d4d718cad56bcb598346282289
nico <nico.ls.asdf@gmail.com>
2026-02-12 11:37:21 -0600

    added people detection to webcam app

---
+++ test_app2_webcam.py
@@ -0,0 +1,152 @@
+import sys
+#import signal
+import subprocess
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
+
+class VentanaScripts(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Detección en Visión Artificial")
+        self.setGeometry(200, 200, 400, 400)
+
+        # Diccionario para almacenar los procesos de cada script
+        self.procesos = {
+            "deteccion_basura_webcam": None,
+            "deteccion_humanos_webcam": None,
+            "deteccion_pez_leon_webcam": None
+        }
+
+        # Layout principal vertical
+        layout_principal = QVBoxLayout()
+
+
+        #Titulo
+        texto_informativo = QLabel("Detección en Visión Artificial")
+        texto_informativo.setWordWrap(True)
+        texto_informativo.setAlignment(Qt.AlignCenter)
+        texto_informativo.setStyleSheet("""
+            font-size: 20px;
+            color: #333;
+            padding: 10px;
+            margin-bottom: 10px;
+
+        """)
+
+        layout_principal.addWidget(texto_informativo)
+
+
+    
+        texto_pasodos = QLabel("Selecciona la detección que deseas aplicar:")
+        texto_pasodos.setWordWrap(True)
+        texto_pasodos.setStyleSheet("""
+            font-size: 15px;
+        """)
+        layout_principal.addWidget(texto_pasodos)
+
+
+        # Scripts
+        layout_principal.addLayout(self.crearSeccion("Detección de basura marina", "/home/cicy2024/detecciones/deteccion_basura_marina_webcam.sh", "deteccion_basura_webcam"))
+        layout_principal.addLayout(self.crearSeccion("Detección de humanos", "/home/cicy2024/detecciones/deteccion_humanos_webcam.sh", "deteccion_humanos_webcam"))
+        layout_principal.addLayout(self.crearSeccion("Detección de pez león", "/home/cicy2024/detecciones/deteccion_pez_leon_webcam.sh", "deteccion_pez_leon_webcam"))
+       
+        # darknet1
+        layout_darknet1 = QHBoxLayout()
+        label_darknet1 = QLabel("Darknet YOLO")
+        layout_darknet1.addWidget(label_darknet1)
+        btn_darknet1 = QPushButton("Abrir Deteccion")
+        btn_darknet1.clicked.connect(self.deteccion_personas)
+        layout_darknet1.addWidget(btn_darknet1)
+        layout_principal.addLayout(layout_darknet1)
+
+        # kill darknet
+        layout_kill = QHBoxLayout()
+        label_kill = QLabel("Cerrar Detección")
+        layout_kill.addWidget(label_kill)
+        btn_kill = QPushButton("Kill Darknet")
+        btn_kill.clicked.connect(self.terminar_proceso)
+        layout_kill.addWidget(btn_kill)
+        layout_principal.addLayout(layout_kill)
+        
+        #Texto manuales
+        texto_manual = QLabel("Encuentra más información sobre las detecciones a continuación:")
+        texto_manual.setWordWrap(True)
+        texto_manual.setStyleSheet("""
+            font-size: 15px;
+        """)
+        layout_principal.addWidget(texto_manual)
+
+         # Manuales
+        btn_abrir_carpeta = QPushButton("Manuales")
+        btn_abrir_carpeta.clicked.connect(self.abrir_carpeta)
+        layout_principal.addWidget(btn_abrir_carpeta)
+
+        #Texto salida
+        texto_salida =  QLabel("Escriba 'Ctrl C' en la terminal para salir de la función de detección")
+        texto_salida.setWordWrap(True)
+        texto_salida.setStyleSheet("""
+            font-size: 15px;
+        """)
+        layout_principal.addWidget(texto_salida)
+
+
+        self.setLayout(layout_principal)
+
+    def crearSeccion(self, nombre_script, ruta_script, clave):
+        layout = QHBoxLayout()
+
+        label = QLabel(nombre_script)
+        layout.addWidget(label)
+
+        btn_ejecutar = QPushButton("Ejecutar")
+        btn_ejecutar.clicked.connect(lambda: self.ejecutar_script(ruta_script, clave))
+        layout.addWidget(btn_ejecutar)
+
+        return layout
+
+
+    def ejecutar_script(self, ruta, clave):
+        if self.procesos[clave] is None:
+            try:
+                self.procesos[clave] = subprocess.Popen(["gnome-terminal", "--", "bash", "-c", f"{ruta}; exec bash"])
+                print(f"{clave} iniciado.")
+            except Exception as e:
+                print(f"Error al ejecutar {clave}: {e}")
+        else:
+            print(f"{clave} ya está en ejecución.")
+
+    #def abrir_vlc(self):
+        #try:
+           # subprocess.Popen(["vlc"])
+           # print("VLC abierto.")
+      #  except Exception as e:
+       #     print("Error al abrir VLC:", e)
+    def deteccion_personas(self):
+        try:
+            subprocess.Popen(["bash", "/home/cicy2024/detecciones/deteccion_humanos2_webcam.sh"])
+        except Exception as e:
+            print("Error al abrir Darknet:", e)
+    def terminar_proceso(self):
+        try:
+            subprocess.Popen(["bash", "/home/cicy2024/detecciones/kill.sh"])
+        except Exception as e:
+            print("Error al terminar proceso:", e)
+
+
+    def abrir_carpeta(self):
+        carpeta_path = "/home/cicy2024/detecciones/Manuales"
+
+        try:
+            subprocess.Popen(["xdg-open", carpeta_path])
+            print("Manuales")
+        except Exception as e:
+            print(f"Error al abrir los manuales: {e}")
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    ventana = VentanaScripts()
+    ventana.show()
+    sys.exit(app.exec_())
